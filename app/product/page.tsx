2de01651fb7a4fe1495bd6c368c511dd57975b82
Jitendra Kumar <jitendra0202006@gmail.com>
2026-07-24 22:06:18 +0530

    Learn to Make Dynamic Routing

---
+++ app/product/page.tsx
@@ -0,0 +1,8 @@
+export default function product(){
+    return (
+        <>
+        <h1>This is the product page</h1>
+        <p>This is the product page desc.</p>
+        </>
+    );
+}
---
+++ app/product/showcase/page.tsx
@@ -0,0 +1,11 @@
+import Button from "@/components/Button";
+
+export default function showcase(){
+    return (
+        <>
+        <h1>This is the showcase page</h1>
+        <p>This is the showcase page desc.</p>
+        <Button/>
+        </>
+    );
+}
