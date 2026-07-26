36de163707c9ef2d62100354b66ae33f584bd3d0
Jitendra Kumar <jitendra0202006@gmail.com>
2026-07-26 19:51:42 +0530

    Learning ServerAction

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,11 @@
+import ServerAction from "@/app/actions/serverAction"
+
 export default function about(){
     return (
         <>
         <h1>This is the about page</h1>
         <p>This is the about page desc.</p>
+        <button onClick={ServerAction}>Click Me</button>
         </>
     );
 }
---
+++ app/actions/serverAction.ts
@@ -0,0 +1,7 @@
+"use server"
+
+// this is server Action which is execute at serverside but call at client side
+
+export default async function ServerAction(){
+    console.log("Action performed");
+}
